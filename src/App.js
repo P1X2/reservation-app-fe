7d1e824f7629412e8296c5610d17ac608f2f6365
wpob <01168909@pw.edu.pl>
2024-10-19 23:24:42 +0200

    Manager employee adding screen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './components/home-page/HomePage';
 import CreateAppointmentForm from './components/CreateAppointmentForm';
 import ServiceList from './components/ServiceList';
 import MyAppointments from './components/MyAppointments';
+import AddEmployeeForm from './components/AddEmployeeForm';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/appointment" element={<CreateAppointmentForm />} />
         <Route path="/services" element={<ServiceList />} />
         <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/manager" element={<AddEmployeeForm />} />
       </Routes>
     </Router>
   );
